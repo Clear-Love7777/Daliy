7ecaa660670999a6f5d0e800476f4f16656715eb
Clear-Love7777 <1601645717@qq.com>
2020-09-14 20:00:37 +0800

    vue

--- vue前端工程化/2.webpack_study/src/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    //编译模式
-    mode:'development', //development  production
-    entry: path.join(_dirname, './src/index.js'),
-    output:{
-        path:join(_dirname, './dist'),//输出文件的存放路径
-       filname:'bundle.js'//输出文件名称
-    }
-};
