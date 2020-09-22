af632285fbaface180d083edd46ae60000e0e249
Clear-Love7777 <1601645717@qq.com>
2020-09-22 23:18:33 +0800

    shop

--- 电商管理/vue_shop/src/main-dev.js
+++ 电商管理/vue_shop/src/main-dev.js
@@ -14,14 +14,26 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+//导入nprogress包对应的js和css
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import axios from 'axios'
+
 //配置请求跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//在request拦截器中展示进度条 NProgress.start()
 axios.interceptors.request.use(config=>{
-  console.log(config)
+  // console.log(config)
+  NProgress.start()
   //在最后必须return config
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+//在response拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config=>{
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
